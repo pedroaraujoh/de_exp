6106937a004221c5b8ba7cdcb33a4bbd7db6116b
pedroaraujoh <pedro.araujo@outlook.com>
2025-02-19 22:05:57 -0300

    aa

--- src/script.sql
+++ src/script.sql
@@ -1,15 +1,19 @@
-create database oficial_db;
+-- Active: 1738546239605@@localhost@5432@db
+-- create database db;
 
-create schema my_schema;
+create schema schema1;
 
-create table oficial_db.my_schema.my_table (
+create table db.schema1.table1 (
 	col1	varchar(100),
 	col2	numeric(14,2)
 );
 
-insert into oficial_db.my_schema.my_table 
-values (
-	'alehandro', '15.2'
-)
+insert into db.schema1.table1 
+values 
+	('maria', 22.5),
+	('jose', 18.75),
+	('ana', 33.2),
+	('pedro', 45.8)
 
-select * from oficial_db.my_schema.my_table
+select * from db.schema1.table1;
+select count(*) from db.schema1.table1
